ddc38c4ce55e02f3e5e8e723836e1e0c3c2a16b6
asmaa.abbas@wework.com <asmaa.abbas@wework.com>
2017-12-29 03:58:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var recipes = {};
 
-function updateObjectWithKeyAndValue(object, key, value) {
+function updateObjectWithKeyAndValue (object, key, value) {
   let newObj = {};
   Object.assign(newObj, object, {[key]: value});
   return newObj;
 }
 
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+  object[key] = value;
+  return object;
+}
